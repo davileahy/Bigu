a07f6861b18181817076e4eb7720d6c641c106d6
_Vito_ <135709641+21vito@users.noreply.github.com>
2024-04-23 22:26:18 -0300

    Alterações no estilo da página
    Foi feita a adição da logo na página principal e do favicon.

--- src/components/locationForm/locationForm.js
+++ src/components/locationForm/locationForm.js
@@ -52,6 +52,7 @@ const LocationForm = () => {
             }
         });
         setPessoaMaisProxima(closestPerson);
+
     };
 
     return (
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,25 +2,20 @@ import React from 'react';
 import { Layout, Typography } from 'antd';
 import LocationForm from '../../components/locationForm/locationForm';
 import 'leaflet/dist/leaflet.css';
+import 'tailwindcss/tailwind.css'; // Certifique-se de importar o CSS do Tailwind
 
 const { Header, Content } = Layout;
 const { Title } = Typography;
 
 function Main() {
     return (
-        <Layout className="layout" style={{ 
-            minHeight: '100vh',
-            background: 'linear-gradient(225deg, #004680, #4484BA)' // Gradiente de fundo
-        }}>
-            <Header style={{ background: 'none', marginBottom: 24 }}>
-                <Title style={{
-                    color: 'white',
-                    textAlign: 'center', 
-                    textShadow: '0 2px 4px rgba(0,0,0,0.5)' // Sombra para melhorar a legibilidade
-                }} level={1}>Bigu</Title>
-            </Header>
-            <Content style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                <div style={{ width: '100%', maxWidth: 600, padding: 24, backgroundColor: 'rgba(255, 255, 255, 0.8)', borderRadius: '8px' }}>
+        <Layout className="flex min-h-screen flex-col bg-gradient-to-br from-blue-800 to-blue-300">
+            <div className="bg-transparent m-3 flex justify-center">
+                <img src="/images/bigulogo.png" alt="logo" className="self-center max-w-full max-h-screen"/>
+            </div>
+
+            <Content className="flex justify-center items-center flex-grow">
+                <div className="w-full max-w-md p-6 bg-white bg-opacity-80 rounded-lg">
                     <LocationForm />
                 </div>
             </Content>
